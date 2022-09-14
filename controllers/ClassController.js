1c9300447cb585fcab99a7248825f3aab4885473
Dewei <tan.dewei@hotmail.com>
2022-09-14 23:35:30 +0800

    add routes for classController

--- controllers/ClassController.js
+++ controllers/ClassController.js
@@ -8,7 +8,6 @@ const Classes = require("../models/Classes");
 const Tutors = require("../models/Tutors");
 const Tutees = require("../models/Tutees");
 const router = express.Router();
-const Classes = require("../models/Classes");
 const moment = require("moment");
 const bodyParser = require("body-parser");
 const SECRET = process.env.SECRET ?? "mysecret";
@@ -19,18 +18,32 @@ router.get("/", (req, res) => {
 
 router.use(bodyParser.json());
 
-router.post("/create-class", async (req, res) => {
-  const newClass = Classes(req.body);
-  await newClass.save();
-  res.sendStatus(201);
-});
+// router.post("/create-class", async (req, res) => {
+//   const newClass = Classes(req.body);
+//   await newClass.save();
+//   res.sendStatus(201);
+// });
+
+// router.get("/get-class", async (req, res) => {
+//   const classes = await Classes.find({
+//     start: { $gte: moment(req.query.start).toDate() },
+//     end: { $lte: moment(req.query.end).toDate() },
+//   });
+//   res.send(classes);
+// });
+
+// router.get("/get-class", async (req, res) => {
+//   const classes = await Classes.find({});
+//   res.send(classes);
+// });
 
 router.get("/get-class", async (req, res) => {
-  const classes = await Classes.find({
-    start: { $gte: moment(req.query.start).toDate() },
-    end: { $lte: moment(req.query.end).toDate() },
-  });
-  res.send(classes);
+  try {
+    const classes = await Classes.find();
+    res.send(classes);
+  } catch (error) {
+    res.status(500).send({ error });
+  }
 });
 
 module.exports = router;
